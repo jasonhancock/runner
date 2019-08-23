61482e0a90344e0e89aed1e7fd829f86073ffdb7
Jason Hancock <jason@jasonhancock.com>
2019-08-23 16:05:14 -0700

    fix gofmt

--- example_test.go
+++ example_test.go
@@ -2,28 +2,28 @@ package runner
 
 import "log"
 
-type myServer struct {}
+type myServer struct{}
 
 func (s *myServer) Run() error {
-    log.Println("starting...")
-    return nil
+	log.Println("starting...")
+	return nil
 }
 
 func (s *myServer) Stop() error {
-    log.Println("stopping...")
-    return nil
+	log.Println("stopping...")
+	return nil
 }
 
 func Example() {
-    // create a context that will be cancelled when an os signal tells the process to shut down.
-    ctx := Context()
+	// create a context that will be cancelled when an os signal tells the process to shut down.
+	ctx := Context()
 
-    // Fire up some background job that will shutdown when ctx is cancelled.
-    go func() {
-        // some background job....
-        <-ctx.Done()
-    }()
+	// Fire up some background job that will shutdown when ctx is cancelled.
+	go func() {
+		// some background job....
+		<-ctx.Done()
+	}()
 
-    // start up a server. This call will block.
-    RunServer(&myServer{})
+	// start up a server. This call will block.
+	RunServer(&myServer{})
 }
